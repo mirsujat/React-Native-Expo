c53d170db0b5b28da4bfb7109cf97fa9fef70c48
mirsujat <mir.sujat@gmail.com>
2018-02-26 20:15:51 +0600

    Implementing PanResponder
    Implementing PanResponder

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder, position };
+  }
+
   renderCards() {
     return this.props.data.map(item => {
       return this.props.renderCard(item);
@@ -9,7 +24,14 @@ class Deck extends Component {
   }
 
   render() {
-    return <View>{this.renderCards()}</View>;
+    return (
+      <Animated.View
+        style={this.state.position.getLayout()}
+        {...this.state.panResponder.panHandlers}
+      >
+        {this.renderCards()}
+      </Animated.View>
+    );
   }
 }
 

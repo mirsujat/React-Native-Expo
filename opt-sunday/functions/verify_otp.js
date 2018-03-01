fba05654010ea48c200898e804b0aa0ab5775d14
mirsujat <mir.sujat@gmail.com>
2018-03-01 15:35:45 +0600

    Invalidating used code
    mir sujat

--- opt-sunday/functions/verify_otp.js
+++ opt-sunday/functions/verify_otp.js
@@ -1,9 +1,27 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
   if (!req.body.phone || !req.body.code) {
     return res.status(422).send({ error: 'Phone and code must be provided' });
   }
 
   const phone = String(req.body.phone).replace(/[^\d]/g, '');
-
   const code = parseInt(code);
+
+  admin
+    .auth()
+    .getUser(phone)
+    .then(() => {
+      const ref = admin.database().ref('users/' + phone);
+      ref.on('value', snapshot => {
+        const user = snapshot.val();
+
+        if (user.code !== code || !user.codeValid) {
+          return res.status(422).send({ error: 'Code not Valid' });
+        }
+
+        ref.update({ codeValid: false });
+      });
+    })
+    .catch(err => res.status(422).send({ error: err }));
 };

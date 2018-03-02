444ff33abeca3dede3810f07fe48e78d1c7dfac1
mirsujat <mir.sujat@gmail.com>
2018-03-02 16:41:20 +0600

    implement axios to get and post user data to firebase database
    mir sujat

--- opt-sunday-client/components/SignUpForm.js
+++ opt-sunday-client/components/SignUpForm.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = '';
 
 class SignUpForm extends Component {
+  state = { phone: '' };
+
+  handleSubmit = async () => {
+    try {
+      await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone });
+      await axios.post(`${ROOT_URL}/requestOtp`, { phone: this.state.phone });
+    } catch (err) {
+      this.setState({ error: err });
+    }
+  };
+
+  //promise implementaion
+  // handleSubmit = () => {
+  //   axios
+  //     .post(`${ROOT_URL}/createUser`, { phone: this.state.phone })
+  //     .then(() => {
+  //       axios.post(`${ROOT_URL}/requestOtp`, { phone: this.state.phone });
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // };
+
   render() {
     return (
       <View>
         <View style={{ marginBottom: 10 }}>
           <FormLabel>Enter Phone Number</FormLabel>
-          <FormInput />
+          <FormInput value={this.state.phone} onChangeText={phone => this.setState({ phone })} />
         </View>
-        <Button title="Submit" />
+        <Button onPress={this.handleSubmit} title="Submit" />
       </View>
     );
   }

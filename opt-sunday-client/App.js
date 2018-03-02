52ae201b606d5dc382dda93be7fea7aa9862c74d
mirsujat <mir.sujat@gmail.com>
2018-03-02 19:24:02 +0600

    singin with phone number completed
    mir sujat

--- opt-sunday-client/App.js
+++ opt-sunday-client/App.js
@@ -1,13 +1,29 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyDzkDeocbdACv9wceEWnyAgb1YDvJvivSs',
+      authDomain: 'otp-sunday.firebaseapp.com',
+      databaseURL: 'https://otp-sunday.firebaseio.com',
+      projectId: 'otp-sunday',
+      storageBucket: 'otp-sunday.appspot.com',
+      messagingSenderId: '1084193189498'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -18,6 +34,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-around'
   }
 });
---
+++ opt-sunday-client/components/SignInForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+import firebase from 'firebase';
+
+const ROOT_URL = '';
+
+class SignInForm extends Component {
+  state = { phone: '', code: '' };
+
+  handleSubmit = async () => {
+    const { phone, code } = this.state;
+    try {
+      let { data } = await axios.post(`${ROOT_URL}/verifyOtp`, { phone, code });
+
+      firebase.auth().signInWithCustomToken(data.token);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    return (
+      <View>
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Phone Number</FormLabel>
+          <FormInput value={this.state.phone} onChangeText={phone => this.setState({ phone })} />
+        </View>
+
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Code</FormLabel>
+          <FormInput value={this.state.code} onChangeText={code => this.setState({ code })} />
+        </View>
+
+        <Button onPress={this.handleSubmit} title="Submit" />
+      </View>
+    );
+  }
+}
+
+export default SignInForm;

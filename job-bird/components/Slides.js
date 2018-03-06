3f571effae8972a27236693e9d6ee4f8d06cb8f8
mirsujat <mir.sujat@gmail.com>
2018-03-06 13:58:07 +0600

    Let's Start Button Added
    Let's Start Button Added > Mir Sujat

--- job-bird/components/Slides.js
+++ job-bird/components/Slides.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
+  renderLastSLide(index) {
+    if (index === this.props.data.length - 1) {
+      return <Button title="Let's Start" raised />;
+    }
+  }
+
+  renderSlides() {
+    return this.props.data.map((slide, index) => {
+      return (
+        <View key={slide.text} style={[styles.slide, { backgroundColor: slide.color }]}>
+          <Text style={styles.slideText}>{slide.text}</Text>
+          {this.renderLastSLide(index)}
+        </View>
+      );
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Text>Slide 1</Text>
-      </View>
+      <ScrollView horizontal pagingEnabled style={{ flex: 1 }}>
+        {this.renderSlides()}
+      </ScrollView>
     );
   }
 }
+
+const styles = {
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: SCREEN_WIDTH
+  },
+  slideText: {
+    fontSize: 30,
+    color: 'white'
+  }
+};
 
 export default Slides;
--- job-bird/screens/WelcomeScreen.js
+++ job-bird/screens/WelcomeScreen.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import Slides from '../components/Slides';
 
+const SLIDE_DATA = [
+  { text: 'Welcome to Job Bird', color: '#03A9F4' },
+  { text: 'Find your dream Job', color: '#009688' },
+  { text: 'Set your location, then swipe away', color: '#03A9F4' }
+];
+
 class WelcomeScreen extends Component {
   render() {
-    return <Slides />;
+    return <Slides data={SLIDE_DATA} />;
   }
 }
 

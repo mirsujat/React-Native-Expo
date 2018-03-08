a348645ea6e5e25c5dca461d0dd415dd9c040292
mirsujat <mir.sujat@gmail.com>
2018-03-08 12:18:13 +0600

    Implement MapView Component
    Implement MapView Component > Mir Sujat

--- job-bird/screens/MapScreen.js
+++ job-bird/screens/MapScreen.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
+import { MapView } from 'expo';
 
 class MapScreen extends Component {
+  state = {
+    mapLoaded: false,
+    region: {
+      longitude: -122.4324,
+      latitude: 37.78825,
+      longitudeDelta: 0.0421,
+      latitudeDelta: 0.0922
+    }
+  };
+
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
+  }
+
   render() {
-    return (
-      <View>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-      </View>
-    );
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+    return <MapView region={this.state.region} style={{ flex: 1 }} />;
   }
 }
 
